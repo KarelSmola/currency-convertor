47c6ddaf2efb525368ae3f14b5206bd2987958a0
Karel Smola <karelsmola21@gmail.com>
2023-08-31 11:28:18 +0200

    Small currency convertor created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState(0);
+  const [currency1, setCurrency1] = useState("EUR");
+  const [currency2, setCurrency2] = useState("CZK");
+  const [result, setResult] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setErrorMessage("");
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${inputValue}&from=${currency1}&to=${currency2}`
+        );
+        if (!response.ok) throw new Error("Change one of the currency");
+        const data = await response.json();
+
+        setResult(data.rates);
+      } catch (error) {
+        setErrorMessage(error.message);
+        return;
+      }
+    };
+
+    fetchData();
+  }, [currency1, currency2, inputValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <select
+        value={currency1}
+        onChange={(e) => {
+          setCurrency1(e.target.value);
+        }}
+      >
+        <option value="CZK">CZK</option>
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+      </select>
+      <select
+        value={currency2}
+        onChange={(e) => {
+          setCurrency2(e.target.value);
+        }}
+      >
+        <option value="CZK">CZK</option>
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+      </select>
+      <p>{errorMessage ? errorMessage : result[currency2]}</p>
     </div>
   );
-}
+};
 
 export default App;
